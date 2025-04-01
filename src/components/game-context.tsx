3179cbbcc848dfc987b43b565aa309801e668238
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-04-01 21:57:37 +0530

    handle message renderers + comment mouse leave logic for now

--- src/components/game-context.tsx
+++ src/components/game-context.tsx
@@ -33,38 +33,38 @@ export function GameProvider({ children }: { children: ReactNode }) {
   const [category, setCategory] = useState<Category | null>(null);
   const [character, setCharacter] = useState<string | null>(null);
   const [characterRevealed, setCharacterRevealed] = useState(false);
-  const [countOfPunishments, setCountOfPunishments] = useState(0);
+  const [, setCountOfPunishments] = useState(0);
 
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    const handleMouseLeave = () => {
-      if (category === null) return;
-      setCountOfPunishments((prev) => {
-        const newCount = prev + 1;
-        toast.error("You will be punished if you leave the game");
-
-        if (newCount > 3) {
-          toast.error(
-            "You have been punished for leaving the game and hence the game has been reset",
-          );
-          setCategory(null);
-          return 0; // Reset punishments count
-        }
-
-        return newCount;
-      });
-    };
-
-    document.documentElement.addEventListener("mouseleave", handleMouseLeave);
-
-    return () => {
-      document.documentElement.removeEventListener(
-        "mouseleave",
-        handleMouseLeave,
-      );
-    };
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return;
+  //
+  //   const handleMouseLeave = () => {
+  //     if (category === null) return;
+  //     setCountOfPunishments((prev) => {
+  //       const newCount = prev + 1;
+  //       toast.warning("You will be punished if you leave the game");
+  //
+  //       if (newCount > 3) {
+  //         toast.error(
+  //           "You have been punished for leaving the game and hence the game has been reset",
+  //         );
+  //         setCategory(null);
+  //         return 0;
+  //       }
+  //
+  //       return newCount;
+  //     });
+  //   };
+  //
+  //   document.documentElement.addEventListener("mouseleave", handleMouseLeave);
+  //
+  //   return () => {
+  //     document.documentElement.removeEventListener(
+  //       "mouseleave",
+  //       handleMouseLeave,
+  //     );
+  //   };
+  // }, [category]);
 
   return (
     <GameContext.Provider
